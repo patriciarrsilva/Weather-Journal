1bfcf2049905fbeed53392ec8f7fcb4a983ff5e3
Patrícia Silva <prrs89@gmail.com>
2020-04-02 22:37:47 +0100

    feat: set up express server

--- server.js
+++ server.js
@@ -2,18 +2,31 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
-/* Middleware*/
+/* Middleware */
+const bodyParser = require('body-parser');
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+const port = 8000;
 
 // Setup Server
+const server = app.listen(port, listening);
+
+// Callback to debug
+function listening() {
+  console.log('server running');
+  console.log(`running on localhost: ${port}`);
+}
