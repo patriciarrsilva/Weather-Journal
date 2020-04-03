5ece20f8a2d3aa657ec6e4c81eacac76537a3f42
Patrícia Silva <prrs89@gmail.com>
2020-04-04 00:19:51 +0100

    feat: update UI based on get and post

--- website/app.js
+++ website/app.js
@@ -1,26 +1,69 @@
 /* Global Variables */
-const baseURL = 'http://api.openweathermap.org/data/2.5/';
 const apiKey = '53cf82947a1f90b5b4a94bda52fd24b6';
+
+const performAction = () => {
+  const zipCode = document.getElementById('zip').value;
+  const feelings = document.getElementById('feelings').value;
+
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${apiKey}`;
+
+  getData(apiUrl).then(data => {
+    console.log('Data from weather API: ', data);
+
+    // Create a new date instance dynamically with JS
+    let d = new Date();
+    let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+    postData('/add', {
+      temperature: data.main.temp,
+      date: newDate,
+      userResponse: feelings
+    });
+  });
+
+  updateUI();
+};
 
 document.getElementById('generate').addEventListener('click', performAction);
 
-const performAction = () => {
-    const zipCode;
-
-    getData();
-};
-
-const getData = async baseURL => {
+const getData = async url => {
   const response = await fetch(url);
 
   try {
     const data = await response.json();
+    console.log(data);
     return data;
   } catch (error) {
-    console.log('error: ', error);
+    console.log('Error: ', error);
   }
 };
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+const postData = async (url = '', data = {}) => {
+  console.log('Data passed to POST on Express', data);
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data)
+  });
+
+  try {
+    const newData = await response.json();
+    return newData;
+  } catch (error) {
+    console.log('Error: ', error);
+  }
+};
+
+const updateUI = async () => {
+  const request = await fetch('/all');
+
+  try {
+    const allData = await request.json();
+
+    document.getElementById('date').innerHTML = allData.date;
+    document.getElementById('temp').innerHTML = allData.temperature;
+    document.getElementById('content').innerHTML = allData.userResponse;
+  } catch (error) {
+    console.log('Error: ', error);
+  }
+};
